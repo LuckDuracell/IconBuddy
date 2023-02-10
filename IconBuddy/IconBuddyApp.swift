46692526ee043a667fed40406216057bcccbb494
LuckDev <lukedru@yahoo.com>
2023-02-09 19:49:00 -0800

    Initial Commit

---
+++ IconBuddy/IconBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconBuddyApp.swift
+//  IconBuddy
+//
+//  Created by Luke Drushell on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct IconBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IconBuddyUITests/IconBuddyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IconBuddyUITestsLaunchTests.swift
+//  IconBuddyUITests
+//
+//  Created by Luke Drushell on 2/9/23.
+//
+
+import XCTest
+
+final class IconBuddyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
